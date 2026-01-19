5782a7899a9cbf1c5d1d60a3828e9845d2110e99
huda shafique <hudashafique19@gmail.com>
2026-01-19 18:09:11 +0500

    check even, odd

--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int num;
+    cout <<"Enter a number :";
+    cin >>num;
+
+    if (num%2 == 0){
+        cout <<"Even number";
+    }
+    else{
+        cout <<"Odd number";
+    }
+    cout <<endl;
+}
